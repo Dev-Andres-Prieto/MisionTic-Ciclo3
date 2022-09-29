db4f2dc5212a9cc3d3af01cee6eb41c501a08e30
Kevin <k.joseph.90@hotmail.com>
2022-09-29 10:24:51 -0500

    Front-End

---
+++ src/main/webapp/js/main.js
@@ -0,0 +1,158 @@
+var email = new URL(location.href).searchParams.get("email");
+var idUser = new URL(location.href).searchParams.get("idUser");
+var user;
+
+$(document).ready(function () {
+
+    $(function () {
+        $('[data-toggle = "tooltip"]').tooltip;
+    });
+
+
+    getUsuario().then(function () {
+        $("#mi-perfil-btn").attr("href", "profile.html?email=" + email + "&idUser=" + idUser);
+        $("#user-saldo").html("$" + user.saldo.toFixed(2)); 
+        getVehiculos(false, "ASC");
+//        $("#oredenar-genero").click(ordenarVehiculos);
+
+    });
+
+});
+
+async function getUsuario() {
+
+    await $.ajax({
+        type: "GET",
+        dataType: "html",
+        url: "./ServletUsuarioPedir",
+        data: $.param({
+            email: email
+        }),
+        success: function (result) {
+            let parsedResult = JSON.parse(result);
+            if (parsedResult != false) {
+                user = parsedResult;                
+            } else {
+                console.log("Error recuperando los datos del usuario");
+            }
+        }
+    });
+}
+
+function getVehiculos(ordenar, orden) {
+
+    $.ajax({
+        type: "GET",
+        dataType: "html",
+        url: "./ServletVehiculoListar",
+        data: $.param({
+            ordenar: ordenar,
+            orden: orden
+        }),
+        success: function (result) {
+            let parsedResult = JSON.parse(result);
+            
+            if (parsedResult != false) {
+                mostrarVehiculos(parsedResult);
+            } else {
+                console.log("No hay vehículos para reservar");
+            }
+        }
+    });
+}
+
+function mostrarVehiculos(vehiculos) {
+
+    let contenido = "";
+
+    $.each(vehiculos, function (index, vehiculo) {
+
+        vehiculo = JSON.parse(vehiculo); 
+        let precio = vehiculo.idTarifa;
+        
+        contenido += '<tr><th scope="row">' + vehiculo.idVehiculo + '</th>' +
+                '<td>' + vehiculo.marca + '</td>' +
+                '<td>' + vehiculo.modelo + '</td>' +
+                '<td>' + vehiculo.idDescripcion + '</td>' +
+                '<td>' + vehiculo.idTarifa + '</td>';
+
+        contenido += '></td>' +
+                //'<td>' + precio + '</td>' +
+                '<td><button onclick="alquilarVehiculo(' + vehiculo.idVehiculo + ',' + precio + ',' + idUser + ');" class="btn btn-success" ';
+        if (user.saldo < precio) {
+            contenido += ' disabled ';
+        }
+
+        contenido += '>Reservar</button></td></tr>';
+
+
+    });
+    $("#vehiculos-tbody").html(contenido);
+}
+
+    function ordenarVehiculos() {
+        if ($("#icono-ordenar").hasClass("fa-sort")) {
+            getVehiculos()(true, "ASC");
+            $("#icono-ordenar").removeClass("fa-sort");
+            $("#icono-ordenar").addClass("fa-sort-down");
+        } else if ($("#icono-ordenar").hasClass("fa-sort-down")) {
+            getVehiculos()(true, "DESC");
+            $("#icono-ordenar").removeClass("fa-sort-down");
+            $("#icono-ordenar").addClass("fa-sort-up");
+        } else if ($("#icono-ordenar").hasClass("fa-sort-up")) {
+            getVehiculos(false, "ASC");
+            $("#icono-ordenar").removeClass("fa-sort-up");
+            $("#icono-ordenar").addClass("fa-sort");
+        }
+    }
+
+    function alquilarVehiculo(idVehiculo, precio, idUser) {
+         console.log("IdUser: " + idUser, "precio: "+precio, "idVehiculo: "+idVehiculo);
+        $.ajax({
+            type: "GET",
+            dataType: "html",
+            url: "./ServletVehiculoAlquilar",
+            data: $.param({
+                id: idVehiculo,
+                email: email,
+                precio: precio,
+                idUser: idUser
+            }),
+            
+            success: function (result) {
+                let parsedResult = JSON.parse(result);
+                console.log("parsedResult desde alquilar vehículo: " +parsedResult);
+                if (parsedResult != false) {
+                   
+                    restarDinero(precio).then(function () {
+                        location.reload();
+                    });
+
+                } else {
+                    console.log("Error en la reserva del vehículo");
+                }
+            }
+        });
+
+    }
+
+    async function restarDinero(precio) {
+        console.log("precio desde restar dinero: " +precio);
+        await $.ajax({
+            type: "GET",
+            dataType: "html",
+            url: "./ServletUsuarioRestarDinero",
+            data: $.param({                
+                idUser: idUser,
+                saldo: parseFloat(user.saldo - precio)
+            }),
+            success: function (result) {
+                let parsedResult = JSON.parse(result);
+                if (parsedResult != false) {
+                    console.log("Saldo actualizado");
+                } else {
+                    console.log("Error en el proceso de pago");
+                }
+            }
+        });
+    }
---
+++ src/main/webapp/js/profile.js
@@ -0,0 +1,215 @@
+var email = new URL(location.href).searchParams.get("email");
+var idUser = new URL(location.href).searchParams.get("idUser");
+var user;
+
+$(document).ready(function () {
+
+
+    fillUsuario().then(function () {
+
+        $("#user-saldo").html("$" + user.saldo.toFixed());
+
+        getReservas(user.email);
+    });
+
+    $("#reservar-btn").attr("href", `home.html?email=${email}&idUser=${idUser}`);
+
+    $("#form-modificar").on("submit", function (event) {
+
+        event.preventDefault();
+        modificarUsuario();
+    });
+
+    $("#aceptar-eliminar-cuenta-btn").click(function () {
+
+        eliminarCuenta().then(function () {
+            location.href = "index.html";
+        });
+    });
+
+});
+
+async function fillUsuario() {
+    await $.ajax({
+        type: "GET",
+        dataType: "html",
+        url: "./ServletUsuarioPedir",
+        data: $.param({
+            email: email
+        }),
+        success: function (result) {
+            let parsedResult = JSON.parse(result);
+
+            if (parsedResult != false) {
+                user = parsedResult;
+                
+                $("#input-nombre").val(parsedResult.nombre);
+                $("#input-apellidos").val(parsedResult.apellido);
+                $("#input-tipo-documento").val(parsedResult.idTipoDocumento);
+                $("#input-num-documento").val(parsedResult.numDocumento);
+                $("#input-tipo-persona").val(parsedResult.idTipoPersona);
+                $("#input-telefono").val(parsedResult.telefono);
+                $("#input-direccion").val(parsedResult.direccion);
+                $("#input-contrasena").val(parsedResult.password);
+                $("#input-email").val(parsedResult.email);
+                $("#input-saldo").val(parsedResult.saldo.toFixed(2));
+
+            } else {
+                console.log("Error recuperando los datos del usuario");
+            }
+        }
+    });
+}
+
+function getReservas(email) {
+
+
+    $.ajax({
+        type: "GET",
+        dataType: "html",
+        url: "./ServletReservaListar",
+        data: $.param({
+            email: email
+        }),
+        success: function (result) {
+            let parsedResult = JSON.parse(result);
+
+            if (parsedResult != false) {
+
+                mostrarHistorial(parsedResult);
+
+            } else {
+                console.log("Error recuperando los datos de las reservas");
+            }
+        }
+    });
+}
+
+function mostrarHistorial(vehiculos) {
+
+    let contenido = "";
+    
+    if (vehiculos.length >= 1) {
+
+        $.each(vehiculos, function (index, vehiculo) {
+
+            vehiculo = JSON.parse(vehiculo);
+            console.log(vehiculo);
+            contenido += '<tr><th scope="row">' + vehiculo.idVehiculo + '</th>' +
+                    '<td>' + vehiculo.modelo + '</td>' +
+                    '<td>' + vehiculo.marca + '</td>';
+
+
+            contenido += '></td><td>' + vehiculo.fechaRecepcion + '</td>' +
+                    '<td><button id="devolver-btn" onclick= "devolverVehiculo(' + vehiculo.idVehiculo
+                    + ', ' + vehiculo.idReserva + ');" class="btn btn-danger">Devolver vehículo</button></td></tr>';
+
+        });
+        $("#historial-tbody").html(contenido);
+        $("#historial-table").removeClass("d-none");
+        $("#historial-vacio").addClass("d-none");
+
+    } else {
+        $("#historial-vacio").removeClass("d-none");
+        $("#historial-table").addClass("d-none");
+    }
+}
+
+
+function devolverVehiculo(idVehiculo, idReserva) {
+
+    $.ajax({
+        type: "GET",
+        dataType: "html",
+        url: "./ServletVehiculoDevolver",
+        data: $.param({
+            idUser: idUser,
+            idVehiculo: idVehiculo,
+            idReserva: idReserva
+        }),
+        success: function (result) {
+
+            if (result != false) {
+
+                location.reload();
+
+            } else {
+                console.log("Error devolviendo el vehículo");
+            }
+        }
+    });
+
+}
+
+function modificarUsuario() {
+
+    let nombre = $("#input-nombre").val();
+    let apellidos = $("#input-apellidos").val();
+    let idTipoDocumento = $("#input-tipo-documento").val();
+    let numDocumento = $("#input-num-documento").val();
+    let idTipoPersona = $("#input-tipo-persona").val();
+    let telefono = $("#input-telefono").val();
+    let direccion = $("#input-direccion").val();
+    let contrasena = $("#input-contrasena").val();
+    let contrasenaConfirmacion = $("#input-contrasena-repeat").val();
+    let email = $("#input-email").val();
+    let saldo = $("#input-saldo").val();
+
+
+    $.ajax({
+        type: "GET",
+        dataType: "html",
+        url: "./ServletUsuarioModificar",
+        data: $.param({
+            nombre: nombre,
+            apellidos: apellidos,
+            idTipoDocumento: idTipoDocumento,
+            numDocumento: numDocumento,
+            idTipoPersona: idTipoPersona,
+            telefono: telefono,
+            direccion: direccion,
+            contrasena: contrasena,
+            email: email,
+            saldo: saldo,
+            idUser: idUser
+        }),
+        success: function (result) {
+
+            if (result != false) {
+                $("#modificar-error").addClass("d-none");
+                $("#modificar-exito").removeClass("d-none");
+            } else {
+                $("#modificar-error").removeClass("d-none");
+                $("#modificar-exito").addClass("d-none");
+            }
+
+            setTimeout(function () {
+                location.reload();
+            }, 3000);
+
+        }
+    });
+
+}
+
+async function eliminarCuenta() {
+
+    await $.ajax({
+        type: "GET",
+        dataType: "html",
+        url: "./ServletUsuarioEliminar",
+        data: $.param({
+            idUser: idUser
+        }),
+        success: function (result) {
+
+            if (result != false) {
+
+                console.log("Usuario eliminado")
+
+            } else {
+                console.log("Error eliminando el usuario");
+            }
+        }
+    });
+}
